ebd4459414226014a85d0caf5e2c0d2d30a84376
sdj3959 <dj9099@naver.com>
2025-08-29 10:12:54 +0900

    3. 레이아웃 사용하기

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -1,20 +1,28 @@
-import {createBrowserRouter} from "react-router-dom";
-import IndexPage from "../pages/IndexPage.jsx";
-import BlogPage from "../pages/BlogPage.jsx";
-import AboutPage from "../pages/AboutPage.jsx";
+import {createBrowserRouter} from 'react-router-dom';
+import IndexPage from '../pages/IndexPage.jsx';
+import BlogPage from '../pages/BlogPage.jsx';
+import AboutPage from '../pages/AboutPage.jsx';
+import RootLayout from '../layouts/RootLayout.jsx';
 
 // 라우터 설정
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <IndexPage />
-  },
-  {
-    path: '/blog',
-    element: <BlogPage />
-  },
-  {
-    path: '/about',
-    element: <AboutPage />
-  },
+    element: <RootLayout />,
+    // children -> Layout의 Outlet부분을 뭘로 바꿀지를 설정
+    children: [
+      {
+        path: '',
+        element: <IndexPage />
+      },
+      {
+        path: 'blog',
+        element: <BlogPage />
+      },
+      {
+        path: 'about',
+        element: <AboutPage />
+      },
+    ]
+  }
 ]);
